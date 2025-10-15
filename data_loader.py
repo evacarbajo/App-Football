511b5b27e9acb415baedb67c762b9f5a3547529f
evacarbajo <evacarbajo>
2025-10-15 14:07:05 +0000

    Connection warehouse Databricks

--- data_loader.py
+++ data_loader.py
@@ -1,34 +1,38 @@
 import streamlit as st
 import pandas as pd
 import sqlite3
-
-#FUNCIÓN PARA CARGAR Y ALMACENAR UN ARCHIVO CSV
-#@st.cache_data
-#def load_data_csv(file_path):
-#   df = pd.read_csv(file_path)
-#    return df
-
-#CONEXION A a SQLite
-@st.cache_resource
-def get_connection():
-    
-    return sqlite3.connect('/Users/evacarbajo/football-data.db',check_same_thread=False) 
-
-#EJECUTA CONSULTA SQL Y DEVUELVE DATAFRAME CACHEADO
-@st.cache_data
-def load_data(query):   
-    conn = get_connection()
-    return pd.read_sql_query(query, conn)
+import os
+from databricks import sql
+from databricks.sdk.core import Config
 
 
 
-"""players = load_data_csv('/Users/evacarbajo/data/players.csv')
-competitions = load_data_csv('/Users/evacarbajo/data/competitions.csv')
-clubs = load_data_csv('/Users/evacarbajo/data/clubs.csv')
-appearances = load_data_csv('/Users/evacarbajo/data/appearances.csv')
-club_games = load_data_csv('/Users/evacarbajo/data/club_games.csv')
-game_events = load_data_csv('/Users/evacarbajo/data/game_events.csv')
-game_lineups = load_data_csv('/Users/evacarbajo/data/game_lineups.csv')
-player_valuations = load_data_csv('/Users/evacarbajo/data/player_valuations.csv')
-transfers = load_data_csv('/Users/evacarbajo/data/transfers.csv')
-games = load_data_csv('/Users/evacarbajo/data/games.csv')"""
+# Verifica que la variable de entorno esta definida
+assert os.getenv('DATABRICKS_WAREHOUSE_ID'), "DATABRICKS_WAREHOUSE_ID must be set in app.yaml."
+
+# Ejecutar consulta SQL en warehouse y devuleve un dataframe
+def sqlQuery(query: str) -> pd.DataFrame:
+    cfg = Config() 
+    #Conexión a warehouse
+    with sql.connect(
+        server_hostname=cfg.host,
+        http_path=f"/sql/1.0/warehouses/{os.getenv('DATABRICKS_WAREHOUSE_ID')}",
+        credentials_provider=lambda: cfg.authenticate
+    ) as connection:
+        #Cursor para ejecutar consulta
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            return cursor.fetchall_arrow().to_pandas()
+
+@st.cache_data()  
+def load_data(query: str):
+    return sqlQuery(query)
+
+
+
+
+
+
+
+
+
